b52670195f91f83407a25e6eacac9ff8c1b19871
Jmz89 <150055331+Jmz89@users.noreply.github.com>
2026-02-20 10:49:51 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
      sys.stdout.write("$ ")
+     user = input()
+     print(f"{user} command not found")
+
 pass
 
 
