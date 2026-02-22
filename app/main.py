1d69068853676edaee6616229da04d481b9d33bf
Jmz89 <150055331+Jmz89@users.noreply.github.com>
2026-02-22 18:22:03 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,39 @@
 import sys
 
+def echo(args=""):
+    print(f"{args}")
+    
+def Practice(args=""):
+    print("This a pa practice command")
+
+#Dictionary of functions
+COMMAND = {
+    "ECHO" : echo,
+    "PRACTICE" : Practice 
+}
+
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-     sys.stdout.write("$ ")
-     user = input()
-     while(user != "exit"):
-        print(f"{user}: command not found")
-        sys.stdout.write("$ ")
-        user = input()
-pass
+     while True:
+        user_input = input("$ ").strip()
+
+        #exits the loop
+        if user_input.lower() == "exit":
+            break
+
+        #Commands and functions
+        parts = user_input.split(" ", 1)
+        command_name = parts[0].upper()
+
+        #This gets the command and everything else after it, if needed
+        args = parts[1] if len(parts) > 1 else ""
+        
+        command_func = COMMAND.get(command_name)
+        if command_func:
+            command_func(args)
+        else:
+            print(f"{user_input} not found")
+            
 
 
 if __name__ == "__main__":
